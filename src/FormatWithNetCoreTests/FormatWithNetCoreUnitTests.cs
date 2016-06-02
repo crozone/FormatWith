960988e04c67eaf877ad52e68f69a7297a9dda3b
crozone <crozone_xz@hotmail.com>
2016-06-02 16:27:07 +1000

    Ported tests over to MSTest

--- src/FormatWithNetCoreTests/FormatWithNetCoreUnitTests.cs
+++ src/FormatWithNetCoreTests/FormatWithNetCoreUnitTests.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Xunit;
 using FormatWith;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace FormatWithTests {
-    // This project can output the Class library as a NuGet Package.
-    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
-    public class Tests {
-
+namespace FormatWithNetCoreUnitTests {
+    [TestClass]
+    public class TestClass {
         public static readonly string Replacement1 = "Replacement1";
         public static readonly string Replacement2 = "Replacement {} Two ";
 
@@ -23,138 +21,138 @@ namespace FormatWithTests {
 
         public static readonly string testFormat4Solution = $"abc{Replacement1}def{{escaped1}}ghi{{{Replacement2}}}jkl{{{{escaped2}}}}mno";
 
-        [Fact]
-        public void TrueTest() {
+        [TestMethod]
+        public void TestMethodPassing() {
             // test to make sure testing framework is working (!)
-            Assert.True(true);
+            Assert.IsTrue(true);
         }
 
-        [Fact]
+        [TestMethod]
         public void TestReplacementEmpty() {
             string replacement = TestFormatEmpty.FormatWith(new { Replacement1 = Replacement1, Replacement2 = Replacement2 });
-            Assert.True(replacement == TestFormatEmpty);
+            Assert.IsTrue(replacement == TestFormatEmpty);
         }
 
-        [Fact]
+        [TestMethod]
         public void TestReplacementNoParams() {
             string replacement = TestFormatNoParams.FormatWith(new { Replacement1 = Replacement1, Replacement2 = Replacement2 });
-            Assert.True(replacement == TestFormatNoParams);
+            Assert.IsTrue(replacement == TestFormatNoParams);
         }
 
-        [Fact]
+        [TestMethod]
         public void TestReplacement3() {
             string replacement = testFormat4.FormatWith(new { Replacement1 = Replacement1, Replacement2 = Replacement2 });
-            Assert.True(replacement == testFormat4Solution);
+            Assert.IsTrue(replacement == testFormat4Solution);
         }
 
-        [Fact]
+        [TestMethod]
         public void TestUnexpectedOpenBracketError() {
             try {
                 string replacement = "abc{Replacement1}{ {Replacement2}".FormatWith(new { Replacement1 = Replacement1, Replacement2 = Replacement2 });
             }
             catch (FormatException e) {
                 if (e.Message == "Unexpected opening brace inside a parameter at position 19") {
-                    Assert.True(true);
+                    Assert.IsTrue(true);
                     return;
                 }
             }
-            Assert.True(false);
+            Assert.IsTrue(false);
         }
 
-        [Fact]
+        [TestMethod]
         public void TestUnexpectedCloseBracketError() {
             try {
                 string replacement = "abc{Replacement1}{{Replacement2}".FormatWith(new { Replacement1 = Replacement1, Replacement2 = Replacement2 });
             }
             catch (FormatException e) {
                 if (e.Message == "Unexpected closing brace at position 31") {
-                    Assert.True(true);
+                    Assert.IsTrue(true);
                     return;
                 }
             }
-            Assert.True(false);
+            Assert.IsTrue(false);
         }
 
-        [Fact]
+        [TestMethod]
         public void TestUnexpectedEndOfFormatString() {
             try {
                 string replacement = "abc{Replacement1}{Replacement2".FormatWith(new { Replacement1 = Replacement1, Replacement2 = Replacement2 });
             }
             catch (FormatException e) {
                 if (e.Message == "The format string ended before the parameter was closed. Position 30") {
-                    Assert.True(true);
+                    Assert.IsTrue(true);
                     return;
                 }
             }
-            Assert.True(false);
+            Assert.IsTrue(false);
         }
 
-        [Fact]
+        [TestMethod]
         public void TestKeyNotFoundError() {
             try {
                 string replacement = "abc{Replacement1}{DoesntExist}".FormatWith(new { Replacement1 = Replacement1, Replacement2 = Replacement2 });
             }
             catch (KeyNotFoundException e) {
                 if (e.Message == "The parameter \"DoesntExist\" was not present in the lookup dictionary") {
-                    Assert.True(true);
+                    Assert.IsTrue(true);
                     return;
                 }
             }
-            Assert.True(false);
+            Assert.IsTrue(false);
         }
 
-        [Fact]
+        [TestMethod]
         public void TestDefaultReplacementParameter() {
             string replacement = "abc{Replacement1}{DoesntExist}".FormatWith(new { Replacement1 = Replacement1, Replacement2 = Replacement2 }, MissingKeyBehaviour.ReplaceWithFallback, "FallbackValue");
 
             if (replacement == "abcReplacement1FallbackValue") {
-                Assert.True(true);
+                Assert.IsTrue(true);
                 return;
             }
 
-            Assert.True(false);
+            Assert.IsTrue(false);
         }
 
-        [Fact]
+        [TestMethod]
         public void TestIgnoreMissingParameter() {
             string replacement = "abc{Replacement1}{DoesntExist}".FormatWith(new { Replacement1 = Replacement1, Replacement2 = Replacement2 }, MissingKeyBehaviour.Ignore);
 
             if (replacement == "abcReplacement1{DoesntExist}") {
-                Assert.True(true);
+                Assert.IsTrue(true);
                 return;
             }
 
-            Assert.True(false);
+            Assert.IsTrue(false);
         }
 
-        [Fact]
+        [TestMethod]
         public void TestCustomBraces() {
             string replacement = "abc<Replacement1><DoesntExist>".FormatWith(new { Replacement1 = Replacement1, Replacement2 = Replacement2 }, MissingKeyBehaviour.Ignore, null, '<', '>');
 
             if (replacement == "abcReplacement1<DoesntExist>") {
-                Assert.True(true);
+                Assert.IsTrue(true);
                 return;
             }
 
-            Assert.True(false);
+            Assert.IsTrue(false);
         }
 
-        [Fact]
+        [TestMethod]
         public void TestGetFormatParameters() {
             List<string> parameters = testFormat4.GetFormatParameters().ToList();
-            if (parameters.Count != 2) Assert.True(false);
+            if (parameters.Count != 2) Assert.IsTrue(false);
             if (parameters[0] != nameof(Replacement1)) {
-                Assert.True(false);
+                Assert.IsTrue(false);
                 return;
             }
             if (parameters[1] != nameof(Replacement2)) {
-                Assert.True(false);
+                Assert.IsTrue(false);
                 return;
             }
-            Assert.True(true);
+            Assert.IsTrue(true);
         }
 
-        [Fact]
+        [TestMethod]
         public void SpeedTest() {
             Dictionary<string, string> replacementDictionary = new Dictionary<string, string>() {
                 ["Replacement1"] = Replacement1,
@@ -164,13 +162,13 @@ namespace FormatWithTests {
             for (int i = 0; i < 1000000; i++) {
                 string replacement = testFormat3.FormatWith(replacementDictionary);
                 if (replacement != testFormat3Solution) {
-                    Assert.True(false);
+                    Assert.IsTrue(false);
                 }
             }
-            Assert.True(true);
+            Assert.IsTrue(true);
         }
 
-        [Fact]
+        [TestMethod]
         public void SpeedTestBigger() {
             Dictionary<string, string> replacementDictionary = new Dictionary<string, string>() {
                 ["Replacement1"] = Replacement1,
@@ -180,21 +178,21 @@ namespace FormatWithTests {
             for (int i = 0; i < 1000000; i++) {
                 string replacement = testFormat4.FormatWith(replacementDictionary);
                 if (replacement != testFormat4Solution) {
-                    Assert.True(false);
+                    Assert.IsTrue(false);
                 }
             }
-            Assert.True(true);
+            Assert.IsTrue(true);
         }
 
-        [Fact]
+        [TestMethod]
         public void SpeedTestBiggerAnonymous() {
             for (int i = 0; i < 1000000; i++) {
                 string replacement = testFormat4.FormatWith(new { Replacement1 = Replacement1, Replacement2 = Replacement2 });
                 if (replacement != testFormat4Solution) {
-                    Assert.True(false);
+                    Assert.IsTrue(false);
                 }
             }
-            Assert.True(true);
+            Assert.IsTrue(true);
         }
     }
 }
