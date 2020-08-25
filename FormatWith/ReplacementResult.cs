db642f5c8dc7a2ec790acbca0cba1acbe843ea0d
Ryan Crosby <ryan.crosby@live.com>
2020-08-25 11:23:05 +1000

    Fix XML documentation

--- FormatWith/ReplacementResult.cs
+++ FormatWith/ReplacementResult.cs
@@ -4,15 +4,33 @@ using System.Text;
 
 namespace FormatWith
 {
+    /// <summary>
+    /// Represents the result of a substitution for a parameter within a format string.
+    /// </summary>
     public struct ReplacementResult
     {
+        /// <summary>
+        /// Represents the result of a substitution for a parameter within a format string.
+        /// </summary>
+        /// <param name="success">Represents whether or not the substitution was successful.</param>
+        /// <param name="value">The new value for the substituted format parameter.</param>
         public ReplacementResult(bool success, object value)
         {
             Success = success;
             Value = value;
         }
 
+        /// <summary>
+        /// Represents whether or not the substitution was successful.
+        /// If true, the handler was successfully able to replace this parameter with the substituted value.
+        /// If false, the substitution failed, and Value will be set to null.
+        /// </summary>
         public bool Success { get; }
+
+        /// <summary>
+        /// The new value for the substituted format parameter.
+        /// If Success is false, this should be set to null.
+        /// </summary>
         public object Value { get; }
     }
 }
